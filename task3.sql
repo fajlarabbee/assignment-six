f3e46e77307e65616968c3647d533a940e00c17e
Fajla Rabbee <thewebdevonline@gmail.com>
2023-11-06 14:45:39 +0600

    Formatted sql built-in codes

--- task3.sql
+++ task3.sql
@@ -1,13 +1,13 @@
 -- Task 3:
 SELECT
-    categories.category_id,
-    categories.name,
-    SUM(order_items.quantity * order_items.unit_price) AS total_revenue
+    cat.category_id,
+    cat.name,
+    SUM(ord_itm.quantity * ord_itm.unit_price) AS total_revenue
 FROM
-    order_items
-    JOIN products ON products.product_id = order_items.product_id
-    JOIN categories ON categories.category_id = products.category_id
+    order_items ord_itm
+    JOIN products p ON p.product_id = ord_itm.product_id
+    JOIN categories cat ON cat.category_id = p.category_id
 GROUP BY
-    products.category_id
+    p.category_id
 ORDER BY
     total_revenue DESC;
