698dd133d9b1bc67ae00eee97fdb0f1b3b5fe99e
Fajla Rabbee <thewebdevonline@gmail.com>
2023-11-06 15:30:34 +0600

    Fixed error

--- task3.sql
+++ task3.sql
@@ -1,12 +1,13 @@
 -- Task 3:
 SELECT
-    cat.name AS category_name,
-    SUM(ord_itm.quantity * ord_itm.unit_price) AS total_revenue
+    cat.category_id,
+    cat.name,
+    IFNULL( SUM(ord_itm.quantity * ord_itm.unit_price), 0) AS total_revenue
 FROM
-    order_items ord_itm
-    JOIN products p ON p.product_id = ord_itm.product_id
-    JOIN categories cat ON cat.category_id = p.category_id
+	categories cat
+	LEFT JOIN products p ON cat.category_id = p.category_id
+	LEFT JOIN order_items ord_itm ON ord_itm.product_id = p.product_id
 GROUP BY
-    p.category_id
+    cat.category_id
 ORDER BY
     total_revenue DESC;
