0ce0298ff09b900cbab55c936b51811fce4b353d
Fajla Rabbee <thewebdevonline@gmail.com>
2023-11-06 14:40:44 +0600

    Formatted

--- task3.sql
+++ task3.sql
@@ -2,12 +2,12 @@
 SELECT
     categories.category_id,
     categories.name,
-    SUM(order_items.quantity * order_items.unit_price) as total_revenue
+    SUM(order_items.quantity * order_items.unit_price) AS total_revenue
 FROM
     order_items
-    join products on products.product_id = order_items.product_id
-    join categories on categories.category_id = products.category_id
-group by
+    JOIN products ON products.product_id = order_items.product_id
+    JOIN categories ON categories.category_id = products.category_id
+GROUP BY
     products.category_id
-order by
-    total_revenue desc;
+ORDER BY
+    total_revenue DESC;
